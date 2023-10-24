ab2594b5348376943c9a9fca2df9bc66198a4fcd
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-10-24 14:50:53 -0300

    create author controllers

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,43 @@
+class AuthorsController < ApplicationController
+    def index
+        @authors = Author.all
+    end
+    def show
+        @author = Author.find(params[:id])
+    end
+    def new
+        @author = Author.new
+        end
+
+     
+   
+    def create
+        author = Author.new(author_params)
+        if author.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+    def edit
+        @book = Book.find(params[:id])
+    end
+    def update
+        author = Author.find(params[:id])
+        if author.update(author_params)
+            redirect_to root_path
+        else
+            render :edit
+        end
+    end
+    def destroy
+        author = Author.find(params[:id])
+        author.destroy
+        redirect_to root_path
+    end
+    private
+    def author_params
+        params.require(:author).permit(:first_name, :last_name, :date_of_birth, :about , :nationality)
+    end
+
+end
