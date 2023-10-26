e04f2bb7125afbc4e04aa2d7e9f60bef6e3d53aa
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-10-26 08:49:34 -0300

    implemtent api books endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "books#index"
+  root 'books#index'
+
+  namespace :api do
+    resources :books
+  end
 end
